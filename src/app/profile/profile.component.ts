b220086ac55d33bf87a3e9e28f38290e40298ca2
Fernando Bueno Gómez <fbueno@arsys.es>
2021-09-10 17:51:39 +0200

    Get user profile from Firestore Database

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { UserProfile } from '../models/user-profile.model';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +12,23 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private itemDoc : AngularFirestoreDocument<UserProfile> | null = null;
+  public item     : Observable<UserProfile> | null               = null;
+  private uid     : string                                       = "";
 
-  constructor(public afAuth: AngularFireAuth, private auth: AuthService, private router: Router) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    private firestore:AngularFirestore,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
+      this.uid = this.route.snapshot.paramMap.get('id') as string;
+    }
 
   ngOnInit() {
+    this.itemDoc = this.firestore.doc<UserProfile>(`users/${this.uid}`);
+    this.item = this.itemDoc.valueChanges() as Observable<UserProfile>;
   }
 
   logout() {
